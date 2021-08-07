04600ffb1f6b6e36b3398a7b41f119db86415cb5
magluar <aryann.magluyan@oocl.com>
2021-08-07 11:53:16 +0800

    [story 3][magluar] feat: should_return_right_car_with_each_ticket_when_fetch_car_twice_given_parking_lot_and_two_parked_cars_to_a_parking_boy

--- src/main/java/com/parkinglot/ParkingBoy.java
+++ src/main/java/com/parkinglot/ParkingBoy.java
@@ -1,14 +1,23 @@
 package com.parkinglot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingBoy {
+    private Map<ParkingTicket, Car> parkedPosittion = new HashMap<>();
+    private Car car;
     public ParkingBoy(ParkingLot parkingLot) {
     }
 
     public ParkingTicket park(Car car) {
-        return new ParkingTicket();
+        ParkingTicket parkingTicket = new ParkingTicket();
+        this.car = car;
+        parkedPosittion.put(parkingTicket, car);
+        return parkingTicket;
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
-        return new Car();
+        return parkedPosittion.get(parkingTicket);
+//        return new Car();
     }
 }
