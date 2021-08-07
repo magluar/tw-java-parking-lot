8c83e2f720fc8affcffbf04c7c9c3fa4d83e53de
magluar <aryann.magluyan@oocl.com>
2021-08-07 21:16:56 +0800

    [story 5][magluar] feat: should_park_to_first_parking_lot_when_park_the_car_given_two_parking_lot_with_first_parking_more_available_position_and_a_car_to_a_parking_boy

---
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -0,0 +1,10 @@
+package com.parkinglot;
+
+import java.util.List;
+
+public class SmartParkingBoy extends ParkingBoy {
+
+    public SmartParkingBoy(List<ParkingLot> parkingLots) {
+        super(parkingLots);
+    }
+}
