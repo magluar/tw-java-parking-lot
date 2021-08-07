083a9d980d304f315226da60256510ad405baf38
magluar <aryann.magluyan@oocl.com>
2021-08-07 11:38:49 +0800

    [story 3][magluar] test: should_return_car_when_fetching_the_car_given_parking_lot_and_parked_car_to_a_parking_boy

--- src/main/java/com/parkinglot/ParkingBoy.java
+++ src/main/java/com/parkinglot/ParkingBoy.java
@@ -7,4 +7,8 @@ public class ParkingBoy {
     public ParkingTicket park(Car car) {
         return new ParkingTicket();
     }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        return null;
+    }
 }
--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -18,4 +18,18 @@ public class ParkingBoyTest {
         //then
         assertNotNull(parkingTicket);
     }
+
+    @Test
+    public void should_return_car_when_fetching_the_car_given_parking_lot_and_parked_car_to_a_parking_boy(){
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+
+        //when
+        Car actualCar = parkingBoy.fetch(parkingTicket);
+
+        //then
+        assertNotNull(actualCar);
+    }
 }
